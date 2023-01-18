bf73ba7f17aa6fe267a5ef749a2a026a78cb3bfd
saracassandra7 <saracassandra7@gmail.com>
2023-01-18 16:26:48 +0100

    [add] seeder

---
+++ app/Models/Project.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    public static function generateSlug($string){
+        $slug = Str::slug($string, '-');
+
+        $original_slug = $slug;
+        $c = 1;
+        $exists = Project::where('slug',$slug)->first();
+        while($exists){
+            $slug = $original_slug . '-' . $c;
+            $exists = Project::where('slug',$slug)->first();
+            $c++;
+        }
+        return $slug;
+    }
+}
